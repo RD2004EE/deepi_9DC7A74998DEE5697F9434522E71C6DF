5e239e9f725e8e02b0d0d17549358c84e9d52d7f
RD2004EE <144421125+RD2004EE@users.noreply.github.com>
2023-10-13 17:35:29 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,15 +1,38 @@
-#player
+'''
+Implement a function called sort_students that takes a list of student objects as input and sorts the
+list based on their CGPA (Cumulative Grade Point Average) in descending order. Each student object
+has the following attributes: name (string), roll_number (string), and cgpa (float). Test the function
+with different input lists of students.
+'''
 
-class Player:
-  def play(self):
-    print("The player is playing cricket.")
-class Batsman(Player):
-   def play(self):
-    print("Thw batsman is batting.")
-class Bowler(Player):
-   def play(self):
-    print("The bowler is bowling.")
-batsman=Batsman()
-bowler=Bowler()
-batsman.play()
-bowler.play()
+class Student:
+
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+
+def sort_students(student_list):
+  # Sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,
+                           reverse=True)
+  # Syntax - lambda arg:exp
+  return sorted_students
+
+student1 = Student("John", "21BCE0987", 3.8)# Example usage:
+students = [
+    Student("Hari", "A123", 7.8),
+    Student("Srikanth", "A124", 8.9),
+    Student("Saumya", "A125", 9.1),
+    Student("Mahidhar", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
+                                                     student.roll_number,
+                                                     student.cgpa))
